6d58b543599ea3b3eca32e20cf56b55b3061037b
Hou <likun.hou@sap.com>
2025-04-16 15:39:52 +0800

    Added garch tools

---
+++ nutest/testscripts/tools/test_garch_tools.py
@@ -0,0 +1,61 @@
+import unittest
+import json
+import numpy as np
+import pandas as pd
+from hana_ai.tools.hana_ml_tools.garch_tools import GARCHFitPredict
+from testML_BaseTestClass import TestML_BaseTestClass
+
+class TestGARCHTools(TestML_BaseTestClass):
+    """
+    Test class for GARCH tools.
+    """
+    tableDef = {
+        '#GARCH_SIM_DATA_TBL':
+            'CREATE LOCAL TEMPORARY TABLE #GARCH_SIM_DATA_TBL ("ID" INTEGER, "DATE" TIMESTAMP, "VAL" DOUBLE)'
+    }
+
+    def setUp(self):
+        super(TestGARCHTools, self).setUp()
+        self._createTable("#GARCH_SIM_DATA_TBL")
+        np.random.seed(3)
+        val = np.random.normal(size=16)
+        dates = pd.date_range(start='2025-01-01', periods=16)
+        data_list = [(i, str(dates[i]),val[i]) for i in range(16)]
+        self._insertData('#GARCH_SIM_DATA_TBL', data_list)
+ 
+    def tearDown(self):
+        self._dropTableIgnoreError("#GARCH_SIM_DATA_TBL")
+        self._dropTableIgnoreError("GARCH_SIM_DATA_TBL_PREDICT_RESULT")
+        super(TestGARCHTools, self).tearDown()
+
+    def test_garch_fit_predict(self):
+        tool = GARCHFitPredict(connection_context=self.conn)
+        tool_input = dict(table_name="#GARCH_SIM_DATA_TBL",
+                          key="ID", endog="VAL",
+                          forecast_length=5)
+        result = json.loads(tool.run(tool_input=tool_input))
+        self.assertTrue(result["garch_predict_result_table"] == "GARCH_SIM_DATA_TBL_PREDICT_RESULT")
+        res1 = self.conn.table("GARCH_SIM_DATA_TBL_PREDICT_RESULT").collect().iloc[:,1]
+        tool_input2 = dict(table_name="#GARCH_SIM_DATA_TBL",
+                           key="DATE", endog="VAL",
+                           forecast_length=5)
+        tool.run(tool_input=tool_input2)
+        res2 = self.conn.table("GARCH_SIM_DATA_TBL_PREDICT_RESULT").collect().iloc[:,1]
+        self.assertTrue(all(x == y for (x,y) in zip(res1, res2)))
+        #test of error message
+        tool_input_err = dict(table_name="#GARCH_SIM_DATA_TBL",
+                              key="TIME_STAMP",
+                              endog="VAL",
+                              forecast_length=5)
+        err_res1 = json.loads(tool.run(tool_input=tool_input_err))
+        self.assertTrue('TIME_STAMP' in err_res1['ValueError occurred'])
+        tool_input_err = dict(table_name="#GARCH_SIM_DATA_TBL",
+                              key="TIMESTAMP",
+                              endog="VAL",
+                              model_type='zgarch',
+                              forecast_length=4)
+        err_res2 = json.loads(tool.run(tool_input=tool_input_err))
+        self.assertTrue('zgarch' in err_res2['ValueError occurred'])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/hana_ai/tools/hana_ml_tools/garch_tools.py
@@ -0,0 +1,150 @@
+"""
+The module defines some agent tools for GARCH class in hana-ml.
+"""
+import json
+import logging
+from typing import Type
+from pydantic import BaseModel, Field, ConfigDict
+
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
+from langchain_core.tools import BaseTool
+
+from hana_ml import ConnectionContext
+from hana_ml.algorithms.pal.tsa.garch import GARCH
+from hana_ai.utility import remove_prefix_sharp
+
+logger = logging.getLogger(__name__)
+
+class GARCHInput(BaseModel):
+    """
+    Class for GARCH parameters (inclusive of fit and predict).
+    """
+    table_name : str = Field(description="The table (or view) containing the data to fit a GARCH model and forecast" +\
+    " the future data variance (risk). If not provided, ask the user, do not guess")
+    # init args
+    key : str = Field(description="the key of the dataset. If not provided, ask the user. Do not guess.")
+    endog : str = Field(description="the column of sequence data to fit the GARCH model and make future predictions." +\
+    " If not provided, ask the user. Do not guess.")
+    p : int = Field(description="Specifies the number of lagged error terms in GARCH model", default=None)
+    q : int = Field(description="Specifies the number of lagged variance terms in GARCH model", default=None)
+    model_type : str = Field(description="Specifies the variant of GARCH model, including 'garch', " +\
+    "'igarch', 'tgarch' and 'egarch'", default=None)
+    thread_ratio : float = Field(description="The ratio of available threads used to fit the GARCH model", default=None)
+    # predict args
+    forecast_length : int = Field(description="The length of future steps to forecast", default=None)
+    model_config = ConfigDict(protected_namespaces=())
+
+class GARCHFitPredict(BaseTool):
+    r"""
+    This tool is used to fit a GARCH model using the input data and then forecast its future variances.
+
+    Parameters
+    ----------
+    connection_context : ConnectionContext
+        Connection context to the HANA database.
+
+    Returns
+    -------
+    str
+        The result string containing the forecast result table name and statistics. 
+
+        .. note::
+
+            args_schema is used to define the schema of the inputs as follows:
+
+            .. list-table::
+                :widths: 15 50
+                :header-rows: 1
+
+                * - Field
+                  - Description
+                * - table_name
+                  - The table (or view) containing the sequence data to fit a GARCH model and make future forecasts on data's volatility.
+                * - key
+                  - The key of the dataset. If not provided, ask the user. Do not guess.
+                * - endog
+                  - The column of sequence data to fit a GARCH model and make future forecasts on data volatility.
+                    If not provided, ask the user. Do not guess
+                * - p
+                  - Specifies the number of lagged error terms in GARCH model.
+                * - q
+                  - Specifies the number of lagged variance terms in GARCH model.
+                * - model_type
+                  - Specifies the variant of GARCH model, including 'garch', 'igarch', 'tgarch' and 'egarch'.
+                * - thread_ratio
+                  - The ratio of available threads used to fit the GARCH model.
+                * - forecast_length
+                  - The length of future steps to forecast.
+    """
+    name: str = "garch_fit_predict"
+    """Name of the tool."""
+    description: str = "To fit a GARCH model on a dataset and then forecast its future volatility."
+    """Description of the tool."""
+    connection_context: ConnectionContext = None
+    """Connection context to the HANA database."""
+    args_schema: Type[BaseModel] = GARCHInput
+    return_direct: bool = True
+
+    def __init__(
+        self,
+        connection_context: ConnectionContext
+    ) -> None:
+        super().__init__(  # type: ignore[call-arg]
+            connection_context=connection_context
+        )
+
+    def _run(#pylint:disable=too-many-positional-arguments
+        self,
+        table_name : str,
+        key : str,
+        endog : str,
+        p : int = None,
+        q : int = None,
+        model_type : str = None,
+        thread_ratio : float = None,
+        forecast_length : int = None,
+        run_manager: CallbackManagerForToolRun = None#pylint:disable=unused-argument
+    ) -> str:
+        """Use the tool."""
+        try:
+            garch = GARCH(p=p, q=q, model_type=model_type)
+            garch.fit(data=self.connection_context.table(table_name)[[key, endog]],
+                      key=key, endog=endog,
+                      thread_ratio=thread_ratio)
+            out_tabs = garch.predict(horizon=forecast_length)
+        except ValueError as verr:
+            return json.dumps({'ValueError occurred': str(verr)})
+        except TypeError as terr:
+            return json.dumps({'TypeError occurred': str(terr)})
+        except KeyError as kerr:
+            return json.dumps({'KeyError occurred': str(kerr)})
+        predict_result = remove_prefix_sharp(f"{table_name}_PREDICT_RESULT")
+        out_tabs[0].save(predict_result, force=True)
+        out_dict = {"garch_predict_result_table" : predict_result}
+        if out_tabs[1].count() > 0:
+            for _, row in out_tabs[1].collect().iterrows():
+                out_dict[row["STAT_NAME"]] = row["STAT_VALUE"]
+        return json.dumps(out_dict)
+
+    async def _arun(#pylint:disable=too-many-positional-arguments
+        self,
+        table_name : str,
+        key : str,
+        endog : str,
+        p : int = None,
+        q : int = None,
+        model_type : str = None,
+        thread_ratio : float = None,
+        forecast_length : int = None,
+        run_manager: AsyncCallbackManagerForToolRun = None
+    ) -> str:
+        return self._run(table_name=table_name,
+                         key=key, endog=endog,
+                         p=p, q=q,
+                         model_type=model_type,
+                         thread_ratio=thread_ratio,
+                         forecast_length=forecast_length,
+                         run_manager=run_manager)
